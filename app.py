1ee4a51aed93fba858ec1bdc9841d2fb23b4f734
Ishaan Phaye <ishaanphaye@gmail.com>
2023-12-19 17:39:38 +0530

    Skeleton frontpage

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 # To run server, use command: flask --app app run --debug
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def hello_html():
+    return render_template('home.html')
