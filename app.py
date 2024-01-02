757310fb5c2b08a78b94e83e9173ba76079b72f6
Ishaan Phaye <ishaanphaye@gmail.com>
2024-01-02 15:31:45 +0530

    Finishing touches + simple API endpoint ("/api/jobs")

--- app.py
+++ app.py
@@ -1,9 +1,41 @@
 # To run server, use command: flask --app app run --debug
 
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+{
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Bengaluru, India',
+    'salary': 'Rs. 10,00,000'
+},
+{
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Pune, India',
+    'salary': 'Rs. 17,00,000'
+},
+{
+    'id': 3,
+    'title': 'Frontend Engineer',
+    'location': 'Remote',
+    # 'salary': 'Rs. 12,00,000'
+},
+{
+    'id': 4,
+    'title': 'Backend Engineer',
+    'location': 'San Francisco, USA',
+    'salary': '$120,000'
+}
+]
+
 @app.route("/")
 def hello_html():
-    return render_template('home.html')
+    return render_template('home.html',jobs=JOBS, company_name='Phi')
+
+# Another way to add dynamic data using Json and API
+@app.route("/api/jobs")
+def list_jobs():
+    return jsonify(JOBS)
